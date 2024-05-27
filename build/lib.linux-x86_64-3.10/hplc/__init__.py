7a4659104257e780f1ff430c203a854e9ed308f4
Konrad1991 <konrad_kraemer@yahoo.de>
2024-05-27 11:50:43 +0200

    removed build folder

--- build/lib.linux-x86_64-3.10/hplc/__init__.py
+++
@@ -1,7 +0,0 @@
-from .read_files import read_chromatograms, read_uv, plot_uv 
-
-__all__ = [
-    'read_chromatograms',
-    'read_uv',
-    'plot_uv'
-]
--- build/lib.linux-x86_64-3.10/xray/__init__.py
+++
@@ -1,5 +0,0 @@
-from .read_files import read_raw  
-
-__all__ = [
-    'read_raw'
-]
