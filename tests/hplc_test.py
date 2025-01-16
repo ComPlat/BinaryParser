e494068e3443dd665658111099d644cd1bdc032d
Konrad1991 <konrad_kraemer@yahoo.de>
2025-01-16 08:48:45 +0100

    update test. So that they are organised as pytest assumes it

---
+++ tests/hplc_test.py
@@ -0,0 +1,17 @@
+import BinaryParser as bp
+
+
+def test_read_chromatograms():
+    path = "./tests/X3346.D"
+    df = bp.read_chromatograms(path)
+
+    assert df.columns.tolist() == [
+        "Wavelength_280",
+        "Wavelength_230",
+        "Wavelength_300",
+        "Wavelength_400",
+        "Wavelength_254",
+        "time",
+    ]
+    assert df.size == 20706
+    assert df.shape == (3451, 6)
